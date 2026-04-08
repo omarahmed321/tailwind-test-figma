98f626217a39849081b9a9787dfff5206497417d
omarahmed321 <omarahmedgodabmox@gmail.com>
2026-04-08 12:28:30 +0200

    start fresh

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+      <header>
+        <img src="" alt="" />
+<nav></nav>
+
+      </header>
+    <main>
+
+    </main>
+    </div>
+  )
+}
