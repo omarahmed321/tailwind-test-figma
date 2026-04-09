c33bab9148b156b77abdba63d5bc5de63b2ef987
Omar Ahmed <omarahmedgodabmox@gmail.com>
2026-04-10 01:51:59 +0200

    deploy: 2026-04-09

--- src/Aitems.jsx
+++ src/Aitems.jsx
@@ -2,14 +2,35 @@ import React from 'react'
 import headphoneitem from './assets/headphoneitem.svg'
 import circleSp from './assets/circlespeaker.svg'
 import speaker from './assets/speaker.svg'
-export default function items() {
+export default function Items() {
   return (
     
-      <div className="items flex gap-7.5">
-    <div className="item w-3 h-2.5 bg-amber-700">
-<img src={headphoneitem} alt="" />
+     
+     
+     <div className="Items flex gap-7.5 w-full justify-between absolute top-30 ">
+
+
+    <div className="item  h-71 bg-amber-700 w-1/3 flex flex-col items-center">
+<img src={headphoneitem} alt="" className=' relative -top-20' />
 <p>HEADPHONES</p>
-<a href=""></a>
+<a href="">SHOP  &gt;</a>
+    </div>
+
+
+    <div className="item  h-71 bg-amber-700  w-1/3 flex flex-col items-center">
+<img src={speaker} alt="" className=' relative -top-20'/>
+<p>SPEAKER</p>
+<a href="">SHOP  &gt;</a>
+    </div>
+
+
+
+    <div className="item  71 bg-amber-700 w-1/3 flex flex-col items-center">
+<img src={circleSp} alt="" className=' relative -top-20'/>
+<p>
+EARPHONE
+</p>
+<a href="">SHOP  &gt;</a>
     </div>
 
 
--- src/Secondpage.jsx
+++ src/Secondpage.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import items from './Aitems'
+
+import Items from './Aitems'
 export default function Secondpage() {
   return (
-    <div className=' justify-center flex flex-col container  bg-amber-600 h-dvh mx-auto'>
-<items />
+    <div className=' justify-center flex flex-col container  bg-amber-600 h-dvh mx-auto relative'>
+<Items />
 
 
 
